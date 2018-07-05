17d28b2a004567d68960d73496398383da655e03
Len <rillera.arnel.e@gmail.com>
2018-07-05 20:28:35 +0800

    Quiz Revisions!

---
+++ dota2quizappsample/webserver/root/quiz/questions.php
@@ -0,0 +1,93 @@
+<?php
+$Questions = array(
+    1 => array(
+        'Question' => 'Crystal Maiden\'s main Attribute is Strength.',
+        'Answers' => array(
+            'A' => 'True',
+            'B' => 'False',
+        ),
+        'CorrectAnswer' => 'A'
+    ),
+    2 => array(
+        'Question' => 'Crystal Maiden\'s main Attribute is:',
+        'Answers' => array(
+            'A' => 'Strength',
+            'B' => 'Agility',
+            'C' => 'Intelligence',
+        ),
+        'CorrectAnswer' => 'C'
+    ),
+    3 => array(
+        'Question' => 'Crystal Maiden\'s first skill is Ice Nova',
+        'Answers' => array(
+            'A' => 'True',
+            'B' => 'False',
+        ),
+        'CorrectAnswer' => 'B'
+    ),
+    4 => array(
+        'Question' => 'Crystal Maiden\'s second skill is Icy Touch',
+        'Answers' => array(
+            'A' => 'True',
+            'B' => 'False',
+        ),
+        'CorrectAnswer' => 'B'
+    ),
+    5 => array(
+        'Question' => 'Crystal Maiden\'s third skill is a passive skill',
+        'Answers' => array(
+            'A' => 'True',
+            'B' => 'False',
+        ),
+        'CorrectAnswer' => 'A'
+    ),
+    6 => array(
+        'Question' => 'Crystal Maiden\'s ultimate skill is Freezing Field',
+        'Answers' => array(
+            'A' => 'True',
+            'B' => 'False',
+        ),
+        'CorrectAnswer' => 'A'
+    ),
+    7 => array(
+        'Question' => 'Crystal Maiden\'s first skill is called',
+        'Answers' => array(
+            'A' => 'Ice Nova',
+            'B' => 'Ice Circle',
+            'C' => 'Frost Circle',
+            'D' => 'Frost Nova',
+        ),
+        'CorrectAnswer' => 'D'
+    ),
+    8 => array(
+        'Question' => 'Crystal Maiden\'s second skill is called',
+        'Answers' => array(
+            'A' => 'Icy Touch',
+            'B' => 'Frostbite',
+            'C' => 'Frost Touch',
+            'D' => 'Icy Bite',
+        ),
+        'CorrectAnswer' => 'B'
+    ),
+    9 => array(
+        'Question' => 'Crystal Maiden\'s third skill is called',
+        'Answers' => array(
+            'A' => 'Magic Aura',
+            'B' => 'Mana Aura',
+            'C' => 'Arcane Aura',
+            'D' => 'Brilliant Aura',
+        ),
+        'CorrectAnswer' => 'C'
+    ),
+    10 => array(
+        'Question' => 'Crystal Maiden\'s ultimate skill is called',
+        'Answers' => array(
+            'A' => 'Icy Field',
+            'B' => 'Freezing Explosions',
+            'C' => 'Freezing Field',
+            'D' => 'Icy Explosions',
+        ),
+        'CorrectAnswer' => 'C'
+    ),
+);
+?>
--- dota2quizappsample/webserver/root/quiz/quiz.php
+++ dota2quizappsample/webserver/root/quiz/quiz.php
@@ -3,55 +3,48 @@
 
 <?php 
 
-$Questions = array(
-    1 => array(
-        'Question' => 'CSS stands for',
-        'Answers' => array(
-            'A' => 'Computer Styled Sections',
-            'B' => 'Cascading Style Sheets',
-            'C' => 'Crazy Solid Shapes'
-        ),
-        'CorrectAnswer' => 'A'
-    ),
-    2 => array(
-        'Question' => 'Second question',
-        'Answers' => array(
-            'A' => 'First answer of Second question',
-            'B' => 'Second answer Second question',
-            'C' => 'Third answer Second question'
-        ),
-        'CorrectAnswer' => 'C'
-    )
-);
+require "questions.php";
 
-if (isset($_POST['answers'])){
+if(isset($_POST['answers'])){
     $Answers = $_POST['answers']; // Get submitted answers.
-
-    // Now this is fun, automated question checking! ;)
-
+    $Score = 0;
+    $Total = 0;
     foreach ($Questions as $QuestionNo => $Value){
-        // Echo the question
-        echo $Value['Question'].'<br />';
+
+//        echo $Value['Question'].'<br />';
 
         if ($Answers[$QuestionNo] != $Value['CorrectAnswer']){
-            echo '<span style="color: red;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span>'; // Replace style with a class
+//            echo '<span style="color: red;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span>';
+            $Total++; 
         } else {
-            echo '<span style="color: green;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span>'; // Replace style with a class
+//            echo '<span style="color: green;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span>';
+            $Score++;
+            $Total++; 
         }
-        echo '<br /><hr>';
+//        echo '<br/><hr>';
     }
-} else {
+    echo '<h4>You got <span style="color:green";>'.$Score. '</span> out of '.$Total.'.';
+} 
+else{
 ?>
-    <form action="grade.php" method="post" id="quiz">
+    <form action="quiz.php" method="post" id="quiz">
     <?php foreach ($Questions as $QuestionNo => $Value){ ?>
     <li>
-        <h3><?php echo $Value['Question']; ?></h3>
+        <h4><?php echo $QuestionNo.". ".$Value['Question']; ?></h4>
         <?php 
             foreach ($Value['Answers'] as $Letter => $Answer){ 
             $Label = 'question-'.$QuestionNo.'-answers-'.$Letter;
         ?>
         <div>
-            <input type="radio" name="answers[<?php echo $QuestionNo; ?>]" id="<?php echo $Label; ?>" value="<?php echo $Letter; ?>" />
+     		<?php
+        	if($Letter == 'A'){
+        		$Checked = ' checked="checked" '; 
+        	}
+        	else{
+        		$Checked = '';
+        	} 
+        	?>
+            <input type="radio" name="answers[<?php echo $QuestionNo; ?>]" id="<?php echo $Label; ?>" value="<?php echo $Letter;?>" <?php echo $Checked; ?> >
             <label for="<?php echo $Label; ?>"><?php echo $Letter; ?>) <?php echo $Answer; ?> </label>
         </div>
         <?php } ?>
